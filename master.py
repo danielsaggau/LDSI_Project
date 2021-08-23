48ed6f02a42e5c1c940f6f886fe00e5e45bf5956
Daniel-Philip Saggau <daniel.saggau@campus.lmu.de>
2021-08-23 20:36:03 +0200

    creating all token exception

--- master.py
+++ master.py
@@ -4,8 +4,8 @@ import re
 import tokenizer
 import numpy as np
 import json
+import tensorflow as tf
 # removing xml markup, symbols and starting text
-
 text = open("/Users/danielsaggau/PycharmProjects/pythonProject/data/output.txt", "r").read()
 text = text.replace("plain_text", " ")
 text = text.replace("FILED", "")
@@ -16,15 +16,12 @@ text = text.replace("*", " ")
 text = text.replace("  ", " ")
 text = text.replace("\uf8fe", " ")
 
-
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 from spacy.lang.en import English
 from spacy.attrs import ORTH, NORM
 nlp = English()
 nlp.add_pipe("sentencizer")
 
 # adding special tokens
-
 nlp.tokenizer.add_special_case('Fed. Cir.', [{ORTH: 'Fed. Cir.'}])
 nlp.tokenizer.add_special_case('9th Cir.', [{ORTH: '9th Cir.'}])
 nlp.tokenizer.add_special_case('See Fed.', [{ORTH: 'See Fed.'}])
@@ -56,6 +53,8 @@ for sent in sentences:
 with open("data/training.json", "w") as fp:
      json.dump(batch, fp)
 
+with open("data/training.txt", "r") as fp:
+     b = json.load(fp)
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 RE_SPLITTER = '<eos>'
@@ -107,8 +106,71 @@ def chunk_text(text, num_tok):
 
     return text_chunk
 
-dat = np.loadtxt("data/sentence_batches.csv")
+b = ''.join(b)
+chunky = chunk_text(b, 256)
 
-chunky = chunk_text(batch, 256)
+with open("data/sentence_chunks.json", "w") as fp:
+    json.dump(chunky, fp)
 
-tokenizer(batch_sentences, padding='max_length', truncation=True)
+flat_list = [item for sublist in chunky for item in sublist]
+
+with open("data/flat_list.json", "w") as fp:
+    json.dump(flat_list, fp)
+
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+# inputs= tokenizer.encode_plus(flat_list, padding='max_length', truncation=True,return_tensors="tf")
+
+input_ids = [tokenizer.encode(sent, return_tensors="tf",truncation=True,padding='max_length') for sent in flat_list]
+tokenized_text = input_ids
+
+examples = []
+block_size = 100
+for i in range(0, len(tokenized_text)-block_size+1, block_size): # Truncate in block of block_size
+    examples.append(tokenized_text[i:i+block_size])
+
+inputs, labels = [], []
+for ex in examples:
+    inputs.append(ex[:-1])
+    labels.append(ex[1:])
+
+from transformers import GPT2Config,TFGPT2LMHeadModel
+config = GPT2Config.from_pretrained('distilgpt2')
+
+model = TFGPT2LMHeadModel.from_pretrained('distilgpt2')
+# splitting into test and training set
+
+dataset = tf.data.Dataset.from_tensor_slices((inputs, labels))
+
+# reference: https://colab.research.google.com/github/peterbayerle/huggingface_notebook/blob/main/distilbert_tf.ipynb#scrollTo=fKTJqUF5R-o4
+
+TEST_SPLIT = 0.2
+BATCH_SIZE = 2
+
+train_size = int(len(inputs) * (1-TEST_SPLIT))
+
+dataset = dataset.shuffle(len(inputs))
+dataset_train = dataset.take(train_size)
+dataset_test = dataset.skip(train_size)
+
+dataset_train = dataset_train.batch(BATCH_SIZE)
+dataset_test = dataset_test.batch(BATCH_SIZE)
+
+
+
+
+
+
+BATCH_SIZE = 2
+BUFFER_SIZE = 10000
+
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+optimizer = tf.keras.optimizers.Adam(learning_rate=3e-5, epsilon=1e-08, clipnorm=1.0)
+loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+metric = tf.keras.metrics.SparseCategoricalAccuracy('accuracy')
+
+model.compile(optimizer=optimizer, loss=[loss, *[None] * model.config.n_layer], metrics=[metric])
+model.fit(dataset, epochs=20)
+
+
