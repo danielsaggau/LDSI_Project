037710a6118af709ea86236e5a543762f6f41688
Daniel-Philip Saggau <daniel.saggau@campus.lmu.de>
2021-08-25 00:45:35 +0200

    trainer

--- trainer.py
+++ trainer.py
@@ -5,29 +5,31 @@ from transformers import AutoConfig, AutoModelForCausalLM
 from transformers import AutoConfig, AutoModelForCausalLM,AutoTokenizer
 import json
 from datasets import load_dataset
-
-datasets = load_dataset("text", data_files={"train": 'data/training.txt'})
-
-
-datasets["train"][0]
+import csv
+import torch
 
 model_checkpoint = "gpt2"
 tokenizer_checkpoint = "sgugger/gpt2-like-tokenizer"
 
 tokenizer = AutoTokenizer.from_pretrained(tokenizer_checkpoint)
 
+#datasets = load_dataset("text", data_files={"train": 'data/training.txt'})
+
+tokenizer.pad_token = tokenizer.eos_token
+input_ids = [tokenizer.encode(sent, return_tensors="pt",truncation=True,padding='max_length') for sent in flat_list]
+
 def tokenize_function(examples):
     return tokenizer(examples["text"])
 
 tokenized_datasets = datasets.map(tokenize_function, batched=True, num_proc=4, remove_columns=["text"])
 
+
+
 training_args = TrainingArguments(
     "test-clm",
     evaluation_strategy = "epoch",
     learning_rate=2e-5,
-    weight_decay=0.01,
-    push_to_hub=True,
-    push_to_hub_model_id=f"{model_checkpoint}-wikitext2",
+    weight_decay=0.01
 )
 
 config = AutoConfig.from_pretrained(model_checkpoint)
@@ -37,7 +39,6 @@ trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=lm_datasets["train"],
-    eval_dataset=lm_datasets["validation"],
 )
 
 trainer.train()
